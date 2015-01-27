4b73611283d82505bac016e3ef57bafdfce09593
donaji13 <toltekochoa12@gmail.com>
2015-01-27 11:54:37 -0800

    got player

--- js/screens/play.js
+++ js/screens/play.js
@@ -6,6 +6,15 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
 
+
+                me.levelDirector.loadLevel("level1");
+                //this calls the level "test"
+
+                var player = me.pool.pull("player", 0, 420, {});
+                me.game.world.addChild(player, 5);
+
+
+                
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
@@ -13,7 +22,7 @@ game.PlayScreen = me.ScreenObject.extend({
 
 
 	/**
-	 *  action to perform when leaving this screen (state change)
+	  *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
